36a60408cf9c8eb94a36eeab8003ae6c6b5cb63e
pdachads <110020160+pdachads@users.noreply.github.com>
2023-01-18 13:42:17 +0530

    Add files via upload

---
+++ base.py
@@ -0,0 +1,55 @@
+# import base64
+
+# a = 'hello'
+
+# b = a.encode()
+
+# c = base64.b64encode(b)
+# print(c)
+
+
+# import urllib.parse
+
+# q = "i will"
+# e = urllib.parse.quote_plus(q)
+
+# print(e)
+
+
+# import html
+
+# a = "<a>"
+
+# c = html.escape(a)
+
+# print(c)
+
+
+# import hashlib
+
+# a= 'geeks'
+
+# print(hashlib.md5(a.encode()).hexdigest())
+
+# import hashlib
+
+# with open('sample.txt','rb') as f:
+#     b =f.read()
+#     print(b)
+
+# print(hashlib.md5(b).hexdigest())
+
+
+
+# import hashlib
+
+# a = 'geeks'
+# c = a.encode()
+# print(c)
+
+# print(hashlib.sha256(c).hexdigest())
+
+
+
+
+
